e185c8ca094cae49a1eb6753b88d107a0f1c4856
BlairAltland <blairaltland@icloud.com>
2016-04-24 17:28:40 -0400

    Minor Fixes
    Formatted the Strings.xml file as well as made some other changes.

---
+++ Hue Calendar copy/hue_calendar/src/main/java/com/nillawaffer/hue/calendar/TagViewActivity.java
@@ -0,0 +1,15 @@
+package com.nillawaffer.hue.calendar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class TagViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alltagactivity);
+    }
+
+}

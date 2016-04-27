8e9ebe8472391346ceae385ad8da81986ae7c1b0
BlairAltland <blairaltland@icloud.com>
2016-04-27 01:10:35 -0400

    Database Event Addition
    It finally works!

--- Hue Calendar copy/hue_calendar/src/main/java/com/nillawaffer/hue/calendar/EventAddition.java
+++ Hue Calendar copy/hue_calendar/src/main/java/com/nillawaffer/hue/calendar/EventAddition.java
@@ -520,16 +520,16 @@ public class EventAddition extends AppCompatActivity {
 
 
                 values.put(Events.SubmitEvent.COLUMN_EVENT_NAME, eventName.getText().toString());
-                values.put(Events.SubmitEvent.COLUMN_EVENT_START_MINUTE, firstMinuteDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_START_HOUR, firstHourDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_END_MINUTE, secondMinuteDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_END_HOUR, secondHourDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_START_YEAR, firstYearDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_START_MONTH, firstMonthDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_START_DAY, firstDayDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_END_YEAR, secondYearDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_END_MONTH, secondMonthDisplay);
-                values.put(Events.SubmitEvent.COLUMN_EVENT_END_DAY, secondDayDisplay);
+                values.put(Events.SubmitEvent.COLUMN_EVENT_START_MINUTE, firstMinuteDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_START_HOUR, firstHourDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_END_MINUTE, secondMinuteDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_END_HOUR, secondHourDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_START_YEAR, firstYearDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_START_MONTH, firstMonthDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_START_DAY, firstDayDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_END_YEAR, secondYearDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_END_MONTH, secondMonthDisplay.getText().toString());
+                values.put(Events.SubmitEvent.COLUMN_EVENT_END_DAY, secondDayDisplay.getText().toString());
                 values.put(Events.SubmitEvent.COLUMN_EVENT_TAGS, tagName.getText().toString());
 
                 // insert the values into the database
@@ -551,7 +551,6 @@ public class EventAddition extends AppCompatActivity {
 
                 eventName.setText("");
                 tagName.setText("");
-                eventRepeat.setText("");
 
             }
         });
